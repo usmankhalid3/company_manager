283d6c0b2242ba95c477c9704a1686c02a55853b
Usman Khalid <usman@favoritemedium.com>
2016-02-16 19:36:26 +0500

    Updating cp in script

--- rest_server/start-server.sh
+++ rest_server/start-server.sh
@@ -4,6 +4,6 @@ echo 'Purging class files..'
 rm -rf bin/
 mkdir bin
 echo 'Compiling code..'
-javac -d bin -sourcepath src -cp lib/gson-2.6.1.jar src/com/rest/server/core/Server.java
+javac -d bin -sourcepath src -cp lib/*.jar src/com/rest/server/core/Server.java
 echo 'Starting server..'
-nohup java -cp bin:lib/gson-2.6.1.jar com.rest.server.core.Server > daemon.out 2>&1 &
+nohup java -cp bin:lib/*.jar com.rest.server.core.Server > daemon.out 2>&1 &
